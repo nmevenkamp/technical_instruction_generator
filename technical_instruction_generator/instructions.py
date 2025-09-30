dee216c1ec358157518806ab3fc8ea3e78f66110
nmevenkamp <niklas.mevenkamp@gmail.com>
2025-10-01 00:11:59 +0200

    refactor page generation

--- technical_instruction_generator/instructions.py
+++ technical_instruction_generator/instructions.py
@@ -44,54 +44,60 @@ class Instructions:
 
         # compile SVGs
         page_idx = 0
-        page, y_group = self._generate_page(page_idx)
-        for step_idx, step in enumerate(self.steps):
-            step_id = step.identifier or f"{step_idx + 1}"
+        page, y0 = self._generate_page(page_idx, self.title)
+        for step_idx in range(len(self.steps)):
+            step_group, h_box = self._generate_step(step_idx)
 
-            w_steps, h_steps = combined_size([step.size for step in self.steps])
-            w_box = A4_WIDTH - MARGIN_LEFT - MARGIN_RIGHT
-            h_box = h_steps + 2 * INSTRUCTION_BOX_PADDING + HEADER_SIZE
-            x_box = -INSTRUCTION_BOX_PADDING
-            y_box = -INSTRUCTION_BOX_PADDING
-
-            group = Group(transform='scale(1,-1)')
-
-            # add box
-            group.append(draw.Rectangle(x_box, y_box, w_box, h_box, fill='white', stroke=INSTRUCTION_BOX_STROKE_COLOR,
-                                        stroke_width=2, rx='5', ry='5'))
-
-            # add instructions
-            y = h_box - INSTRUCTION_BOX_PADDING - HEADER_SIZE
-            group.append(draw.Line(x_box, y, x_box + w_box, y, stroke=INSTRUCTION_BOX_STROKE_COLOR))
-            text = Text(
-                f"Schritt {step_id}:",
-                FONT_SIZE_BASE,
-                -INSTRUCTION_BOX_PADDING + HEADER_TEXT_OFFSET_X,
-                y + HEADER_SIZE / 2,
-                dominant_baseline="middle",
-                font_weight='bold',
-                font_family=FONT_FAMILY_TEXT,
-            )
-            text.append(draw.TSpan(step.instruction, dx=5, font_weight='normal'))
-            group.append(text)
-
-            # add steps
-            # TODO: only add steps performed on the same face of the same part
-            for step_ in self.steps[:-step_idx]:
-                step_.draw(group, active=False)
-            step.draw(group, active=True)
-
-            if y_group + h_box > A4_HEIGHT - MARGIN_BOTTOM:
+            if y0 + h_box > A4_HEIGHT - MARGIN_BOTTOM:
                 pages.append(page)
                 page_idx += 1
-                page, y_group = self._generate_page(page_idx)
+                page, y0 = self._generate_page(page_idx)
 
-            page.append(draw.Use(group, MARGIN_LEFT, y_group + h_box))
-            y_group += h_box + INSTRUCTION_BOX_MARGIN
+            page.append(draw.Use(step_group, MARGIN_LEFT, y0 + h_box))
+            y0 += h_box + INSTRUCTION_BOX_MARGIN
 
         pages.append(page)
 
         return pages
+
+    def _generate_step(self, step_idx: int) -> tuple[Group, float]:
+        step = self.steps[step_idx]
+        step_id = step.identifier or f"{step_idx + 1}"
+
+        w_steps, h_steps = combined_size([step.size for step in self.steps])
+        w_box = A4_WIDTH - MARGIN_LEFT - MARGIN_RIGHT
+        h_box = h_steps + 2 * INSTRUCTION_BOX_PADDING + HEADER_SIZE
+        x_box = -INSTRUCTION_BOX_PADDING
+        y_box = -INSTRUCTION_BOX_PADDING
+
+        group = Group(transform='scale(1,-1)')
+
+        # add box
+        group.append(draw.Rectangle(x_box, y_box, w_box, h_box, fill='white', stroke=INSTRUCTION_BOX_STROKE_COLOR,
+                                    stroke_width=2, rx='5', ry='5'))
+
+        # add instructions
+        y = h_box - INSTRUCTION_BOX_PADDING - HEADER_SIZE
+        group.append(draw.Line(x_box, y, x_box + w_box, y, stroke=INSTRUCTION_BOX_STROKE_COLOR))
+        text = Text(
+            f"Schritt {step_id}:",
+            FONT_SIZE_BASE,
+            -INSTRUCTION_BOX_PADDING + HEADER_TEXT_OFFSET_X,
+            y + HEADER_SIZE / 2,
+            dominant_baseline="middle",
+            font_weight='bold',
+            font_family=FONT_FAMILY_TEXT,
+        )
+        text.append(draw.TSpan(step.instruction, dx=5, font_weight='normal'))
+        group.append(text)
+
+        # add steps
+        # TODO: only add steps performed on the same face of the same part
+        for step_ in self.steps[:-step_idx]:
+            step_.draw(group, active=False)
+        step.draw(group, active=True)
+
+        return group, h_box
 
     def _generate_page(self, page_idx: int = None, title: str = None) -> tuple[Drawing, float]:
         w_canvas = A4_WIDTH
